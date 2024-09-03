2ac53e34dd7d74bf969bac82cfe2b70f20cd3bd0
NehalKaneria1501 <158566044+NehalKaneria1501@users.noreply.github.com>
2024-09-03 18:29:21 +0530

    Add files via upload

---
+++ ExplicitDataTypeConversion.java
@@ -0,0 +1,14 @@
+public class ExplicitDataTypeConversion
+{
+    public static void main(String[] args)
+    {
+        double a=50.50;
+        System.out.println("Double representation"+a);
+        float f=(float)a;
+        System.out.println("Float representsation"+f);
+        long b=(long)a;
+        System.out.println("Long representation"+b);
+        int c=(int)b;
+        System.out.println("FLoat representarion"+c);
+    }
+}
